1c6e8a2b4d44e84b27ccae9226e9893d10a2ce97
TAKUMI-OWARI <awsmk12@i.softbank.jp>
2019-06-16 15:42:29 +0900

    Create group model and group controller

--- db/migrate/20190602052717_add_devise_to_users.rb
+++ db/migrate/20190602052717_add_devise_to_users.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class DeviseCreateUsers < ActiveRecord::Migration[5.0]
+class AddDeviseToUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
@@ -34,7 +34,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
       # t.datetime :locked_at
 
 
-      t.timestamps null: false
+      # Uncomment below if timestamps were not included in your original model.
+      # t.timestamps null: false
     end
 
     add_index :users, :email,                unique: true
